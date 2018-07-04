1f5b17834c2c2c0923f88847464fb688200488d7
Marek Meyer <marek_meyer@outlook.com>
2018-07-04 16:09:16 +0100

    Display proper track names with artist

--- features/Main/TopTracks.js
+++ features/Main/TopTracks.js
@@ -6,10 +6,34 @@ import FlatListView from './FlatListView.js';
 
 class TopTracks extends React.Component {
 
+  getArtistNames = (artists) => {
+    
+    // TODO test this
+    let artistString = ''
+    for (let i = 0; i < artists.length; i++) {
+      const artist = artists[i];
+      if (i > 1 && artists.length > 4) return `${artistString} and others`
+      if (i === 0) {
+        artistString = artist.name;
+      }else if (i === artists.length - 1) {
+        artistString = `${artistString} & ${artist.name}` 
+      }
+    }
+    return artistString
+  }
+
   render() {
 
     // TODO do name stuff for tracks
-    const topTracks = this.props.topTracks;
+    const topTracks = this.props.topTracks.map(track => {
+      const artist = this.getArtistNames(track.artists);
+      const name = `${artist}, ${track.name}`
+      return {
+        id: track.id,
+        name: name
+      }
+    });
+
     console.log('props', topTracks)
     return (
       <View>
