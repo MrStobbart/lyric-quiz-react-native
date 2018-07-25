8b860e819f368d4692aa2533f8732097b6e5563a
Marek Meyer <marek_meyer@outlook.com>
2018-07-25 11:55:44 +0100

    Implemented check for internet connection on startup

--- features/Auth/AuthInitial.js
+++ features/Auth/AuthInitial.js
@@ -1,20 +1,54 @@
 import React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, NetInfo } from 'react-native';
 
 
 export default class AuthInitial extends React.Component{
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasInternetConnection: false
+    }
+  }
+
+  componentDidMount() {
+
+    NetInfo.addEventListener(
+      'connectionChange',
+      this.handleInternetConnectionChange
+    );
+
+    NetInfo.getConnectionInfo()
+      .then(connectionInfo => this.handleInternetConnectionChange(connectionInfo))  
+  }
+
+  handleInternetConnectionChange = (connectionInfo) => {
+    console.log('Connection change, type: ' + connectionInfo.type + ', effectiveType: ' + connectionInfo.effectiveType);
+    if (connectionInfo.type !== 'none') {
+      console.log('it is not none')
+      this.setState({hasInternetConnection: true})
+    }
+  }
 
   // TODO style this propery
   render() {
-    return (
-      <View>
-        <Text>This is the initial View</Text>
-        <Button
-          title="Login"
-          onPress={() => this.props.navigation.navigate('AuthSpotify')}
-        />
-      </View>
-    )
+    if (this.state.hasInternetConnection) {
+      return (
+        <View>
+          <Text>This is the initial View</Text>
+          <Button
+            title="Login"
+            onPress={() => this.props.navigation.navigate('AuthSpotify')}
+          />
+        </View>
+      )  
+    } else {
+      return (
+        <View>
+          <Text>This app needs an active internet connection</Text>
+        </View>
+      )
+    }
+    
   }
 }
