713ad4f853152a7a9750c4456dadace37f67511f
Marek Meyer <marek_meyer@outlook.com>
2018-08-07 20:31:16 +0100

    Styled Improved FlatListView

--- features/Main/FlatListView.js
+++ features/Main/FlatListView.js
@@ -6,12 +6,54 @@ export default function FlatListView(props) {
   return (
     <FlatList
       data={props.data}
-      renderItem={({ item }) => <Text key={item.id}>{item.name}</Text>}
+      renderItem={({ item, index }) => <Item key={item.id} index={index}>{item.name}</Item>}
       keyExtractor={(item, index) => index.toString()}
+      ItemSeparatorComponent={() => <ItemSeperator />}
     />
   )
 }
 
+
+function Item(props) {
+  return (
+    <View
+      style={{
+        margin: 18,
+        flex: 1,
+        flexDirection: 'row'
+      }}
+    >
+      <Text
+        style={{
+          flex: 1,
+          width: 60,
+        }}
+      >
+        {props.index + 1}
+      </Text>
+      <Text
+        style={{
+          textAlign: 'left',
+          flex: 10
+        }}
+      >
+        {props.children}
+      </Text>
+    </View>
+  )
+}
+
+function ItemSeperator(props) {
+  return (
+    <View
+      style={{
+        backgroundColor: 'grey',
+        opacity: 0.5,
+        height: 1
+      }}
+    />
+  )
+}
 
 // TODO styling
 
